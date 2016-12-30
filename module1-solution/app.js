6b5125bf2f24e537632220a87a677acbab1cd2c2
itelemarked <itelemarked@icloud.com>
2016-12-30 21:30:08 +0100

    advanced1
    assignment 1 (advanced)

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,24 +1,35 @@
 (function() {
   'use strict'
 
-  angular.module('LunchCheckApp', [])
+  angular.module('LunchCheck', [])
 
   .controller('LunchCheckCtrl', LunchCheckCtrl);
 
+  LunchCheckCtrl.$inject=['$scope'];
   function LunchCheckCtrl($scope) {
+
+    $scope.myColor = {};
 
     $scope.check = function() {
       var arr = [];
 
       if(!$scope.input) {
         $scope.message = 'Please enter data first';
+        $scope.myColor = {color: 'red'};
       } else {
         arr = $scope.input.split(',');
-        arr.length < 4 ? $scope.message = 'Enjoy!' : $scope.message = 'Too much!';
+        getLengthWithoutBlank(arr) < 4 ? $scope.message = 'Enjoy!' : $scope.message = 'Too much!';
+        $scope.myColor = {color: 'green'};
       }
     }
-
   }
 
+  function getLengthWithoutBlank(arr) {
+    var l = arr.length;
+    angular.forEach(arr, function(val, i) {
+      if(val === '' || val === ' ') l -= 1;
+    });
+    return l;
+  }
 
 })();
