d1db3635eb1e1cd824b6f8ae703171da8e9afe81
itelemarked <itelemarked@icloud.com>
2017-01-23 15:58:46 +0100

    simulation of 1 sec delay to get categories

--- module4-solution/src/dataModule/menudata.service.js
+++ module4-solution/src/dataModule/menudata.service.js
@@ -4,13 +4,24 @@
   angular.module('data')
   .service('MenuDataService', MenuDataService);
   
-  MenuDataService.$inject = ['$http'];
-  function MenuDataService($http) {
+  MenuDataService.$inject = ['$http', '$q', '$timeout'];
+  function MenuDataService($http, $q, $timeout) {
     var self = this;
     
-    self.getAllCategories = function() {
-    	return $http.get('https://davids-restaurant.herokuapp.com/categories.json');
-      // https://davids-restaurant.herokuapp.com/categories.json
+//     self.getAllCategories = function() {
+//     	return $http.get('https://davids-restaurant.herokuapp.com/categories.json');
+//     };
+
+		self.getAllCategories = function() {
+			var deferred = $q.defer();
+						
+			$timeout(function() {
+				$http.get('https://davids-restaurant.herokuapp.com/categories.json').then(function(result) {
+					deferred.resolve(result);
+				});
+			}, 1000);
+    	
+    	return deferred.promise;
     };
     
     self.getItemsForCategory = function(categoryShortName) {
